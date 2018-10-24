0ce1cb2f7cd19f77084f8d4dd460647b3607b32f
Jonathan <jonathan@iMac9.local>
2018-10-24 11:10:50 -0400

    Keep writing the program. Trying to use loops to remove numbers from the list

--- assignment_6.py
+++ assignment_6.py
@@ -1,7 +1,18 @@
-limit = int(input("Max number = ? "))
-number = 2
-list_one = ["2"]
-while number < limit:
-    number = number + 1
-    list_one.append("number")
-print(list_one)
+def list():
+    limit = int(input("Max number = ? "))
+    number = 2
+    list_one = [2]
+    list_two = []
+    while number < limit:
+        number = number + 1
+        list_one.append(number)
+    print(list_one)
+    first = list_one[0]
+    list_two.append(first)
+    for number in list_one:
+        if number % first == 0:
+            list_one.remove(number)
+    print([list_two])
+
+list()
+
