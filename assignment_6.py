8c7dcf65e135f24778aa6a829d3d2ba0de8aadc6
Jonathan <jonathan@iMac9.local>
2018-10-26 13:29:42 -0400

    Writing comments for the function

--- assignment_6.py
+++ assignment_6.py
@@ -1,12 +1,14 @@
-def list():
+def main():
     limit = int(input("Max number = ? "))
     number = 2
-    list_one = [2]
+    list_one = [2]  # The first number of the original list
     list_two = []
+    # The second list, which there is going to be only prime numbers in it.
+    # It is currently a blank list.
     while number < limit:
         number = number + 1
-        list_one.append(number)
-    while len(list_one) > 0:
+        list_one.append(number)  # Create list_one by putting in consecutive numbers added by 1
+    while len(list_one) > 0:  # While the 
         first = list_one[0]
         list_two.append(first)
         for number in list_one:
@@ -14,4 +16,5 @@ def list():
                 list_one.remove(number)
     print(list_two)
 
-list()
+
+main()
