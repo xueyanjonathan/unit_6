86f0701d1f6e66d792668762fb12584cdbddf672
Jonathan <jonathan@iMac9.local>
2018-10-26 13:44:38 -0400

    Finishing all comments and the title of the function

--- assignment_6.py
+++ assignment_6.py
@@ -1,3 +1,9 @@
+# Jonathan Lin
+# 10/26/2018
+# List
+# Using lists and loops to create The Sieve of Eratosthenes
+
+
 def main():
     limit = int(input("Max number = ? "))
     number = 2
@@ -8,11 +14,13 @@ def main():
     while number < limit:
         number = number + 1
         list_one.append(number)  # Create list_one by putting in consecutive numbers added by 1
-    while len(list_one) > 0:  # While the 
+    while len(list_one) > 0:  # While there are still numbers in the original list, the loop will continue.
         first = list_one[0]
-        list_two.append(first)
-        for number in list_one:
+        list_two.append(first)  # Add the first number, which will be the prime number in list one, into list two.
+        for number in list_one:  # Looping all numbers
             if number % first == 0:
+                # If a number in the list divided by the first number has no remainder,
+                # it is the multiple of the prime number, and it will be removed from the list.
                 list_one.remove(number)
     print(list_two)
 
